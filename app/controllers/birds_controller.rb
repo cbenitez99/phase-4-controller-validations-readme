c6ab4868560428051ab021b133bf69b01b62ba4c
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-11-22 12:57:08 -0600

    commit tree 03abe890e024925cff18a7eb7a74705fb1321e59

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,16 +1,12 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
   end
 
   # GET /birds/:id
@@ -19,12 +15,40 @@ class BirdsController < ApplicationController
     render json: bird
   end
 
-  # PATCH /birds/:id
+  # POST /birds :
+
+  # def create
+  #   bird = Bird.create!(bird_params)
+  #   render json: bird, status: :created
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  # end
+
+  #  OR && w/o line 3
+
+  def create
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
+    render json: bird, status: :created
+  end
+
+  # PATCH /birds/:id :
+
+  # def update
+  #   bird = find_bird
+  #   bird.update!(bird_params)
+  #   render json: bird
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
+
+  #  OR && w/o line 3
   def update
     bird = find_bird
-    bird.update(bird_params)
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
     render json: bird
-  end
+  end 
 
   # DELETE /birds/:id
   def destroy
@@ -45,6 +69,13 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    
+    #render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity 
+    #=> {"errors": ["Name can't be blank", "Species must be unique"]} 
   end
 
 end
